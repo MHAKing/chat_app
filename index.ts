821835a96fc59cb9363ae5ea41da3a425ad243aa
MuhammadHassanAli-VRD <hassanali@virtualrecruitmentdays.com>
2023-06-23 19:09:27 +0500

    add port change

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import WebSocket, { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 3000 });
+let port_num = process.env.PORT || 3000;
+const wss = new WebSocketServer({ port: port_num });
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function message(message:string) {
     const data = JSON.parse(message);
-    console.log(data);
+    console.log(data); 
     if (data.type === "message") {
+      console.log(wss.clients);
       wss.clients.forEach((client) => {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
           client.send(JSON.stringify({ type: "message", data: data.data }));
